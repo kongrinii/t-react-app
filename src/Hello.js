cab4a0c66d7ccc8b2be04543a4f29a678cc916bd
kongrinii <dhrudfls1031@naver.com>
2023-04-18 12:12:48 +0900

    src 6장 React 로 안녕하세요! 출력하기 다시 한 것

--- src/Hello.js
+++ src/Hello.js
@@ -1,6 +1,5 @@
 import React from 'react';
-class Hello extends React.Component { 
-    render(){
+class Hello extends React.Component { render(){
     return (
         <div className="Hello">
         <label for="hello"> 안녕하세요!</label>
