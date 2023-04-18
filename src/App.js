56c9bd8cd178aa4fb780af8f333fd5103ad292df
kongrinii <dhrudfls1031@naver.com>
2023-04-18 13:25:43 +0900

    10장. React 로 목록 추가, 수정, 삭제하기

--- src/App.js
+++ src/App.js
@@ -1,34 +1,65 @@
 import React from 'react';
 import Todo from './Todo';
-import AddTodo from './AddTodo';
-import {Paper, List, Container}from "@material-ui/core";
+import Addtodo from './AddTodo';
+import {Paper, List, Container} from "@material-ui/core";
 import './App.css';
-class App extends React.Component { constructor(props){ //매개변수 props 생성자
- super(props); // 매개변수 pros 초기화
- this.state ={ // item 에 item.id, item.title, item.done 매개변수 이름과 값 할당
- items :[ {id:"todo0", title:"Todo 1 ", done:true}, {id:"todo1", title:"Todo 2 ", done:false}, ], }; } // (1) add 함수 추가
- add =(item)=>{ const thisItems =this.state.items; item.id ="ID-"+ thisItems.length;//key를 위한 id 추가
- item.done =false; thisItems.push(item); this.setState({items:thisItems});//update state
- console.log("items:",this.state.items);
- } 
- render(){ // todoItems에 this.state.items.length 가 0보다 크다면 true 이므로 && 뒤에 값을 넘겨준다. // totoItem = this.state.items.length > 0 ? (<Paper></Paper>):""; 이렇게 해도 같은 결과이다. 조건선택문 ? ternary operator
- var todoItems =this.state.items.length >0 &&(
- <Paper style={{margin:16}}>
- <List>
- {this.state.items.map((item,idx)=>(
- <Todo item={item} key={item.id}/>
- ))} </List>
- </Paper>
- );
- // (2) add 함수 연결
- return (
- <div className="App">
- <Container maxWidth="md">
- <AddTodo add={this.add}/>
- <div className="TodoList">{todoItems}</div>
 
- </Container>
- </div>
- ); }
+class App extends React.Component {
+  constructor(props) {   // 매개변수 props 생성자 
+    super(props);  // 매개변수 props 초기화
+    this.state = {
+      items : [
+        {id: 0, title: "Todo 1 ", done: true},  // item에 item.id, item.title, item.done 매개변수 이름과 값을 할당
+        {id: 1, title: "Todo 2", done: false},
+      ],
+    };
+  }
+
+  // add 함수 추가
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-"+thisItems.length;  // key를 위한 id 추가
+    item.done = false;
+    thisItems.push(item);
+    this.setState({items: thisItems});  // update state
+    console.log("items: ", this.state.items);
+  }
+
+  // delete 함수 추가 (alt+shift+f 포맷팅)
+  delete = (item) => {
+    const thisItems = this.state.items;
+    const newItems = thisItems.filter(e => e.id !== item.id);
+    this.setState({items: newItems}, () => {
+      // 디버깅 콜백
+      console.log("Update Items: ", this.state.items)
+    });
+  }
+
+  render() {
+    // todoItems에 this.state.items.length > 0 이면 true => && 뒤에 값을 넘겨준다.
+    // -> todoItems = this.state.items.length > 0 ? (<Paper></Paper>): ""; 으로 작성해도 같은 결과(? : 조건 선택문 사용한 ver.)
+
+    // javascript에서 제공하는 map 함수를 이용해 배열을 반복하여 <Todo/> Component를 여러 개 생성할 수 있다.
+    var todoItems = this.state.items.length > 0 && (
+      <Paper style={{margin: 16}}>
+        <List>
+          {this.state.items.map((item, idx) => (
+                  <Todo item={item} key={item.id} delete={this.delete}/>
+          ))}
+        </List>
+      </Paper>
+    );
+
+    // 생성된 Component JSX를 리턴
+    return (
+      <div className='App'>
+        <Container maxWidth="md">
+          <Addtodo add={this.add}/>
+          <div className='TodoList'>{todoItems}</div>
+        </Container>
+      </div>
+    )
+
+  }
 }
 export default App;
